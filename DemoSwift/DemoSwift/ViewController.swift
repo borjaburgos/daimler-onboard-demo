b908a62322bdcbd107705fbb4bf257eff54f0f63
Fernando Mayo <fermayo@gmail.com>
2018-10-14 18:27:45 -0700

    Add Xcode project
    Signed-off-by: Fernando Mayo <fermayo@gmail.com>

---
+++ DemoSwift/DemoSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoSwift
+//
+//  Created by Fernando Mayo Fernandez on 10/14/18.
+//  Copyright © 2018 Fernando Mayo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
